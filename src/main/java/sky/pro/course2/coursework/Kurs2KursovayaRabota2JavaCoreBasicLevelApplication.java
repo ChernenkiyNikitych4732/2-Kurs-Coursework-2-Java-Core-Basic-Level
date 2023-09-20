157506962af7e728b96b396f6d44558bd25a356d
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-09-20 14:41:37 +0200

    2 Курс Курсовая работа №2 Java Core изначальный комментарий

---
+++ src/main/java/sky/pro/course2/coursework/Kurs2KursovayaRabota2JavaCoreBasicLevelApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course2.coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2KursovayaRabota2JavaCoreBasicLevelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2KursovayaRabota2JavaCoreBasicLevelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course2/coursework/Kurs2KursovayaRabota2JavaCoreBasicLevelApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course2.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2KursovayaRabota2JavaCoreBasicLevelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
